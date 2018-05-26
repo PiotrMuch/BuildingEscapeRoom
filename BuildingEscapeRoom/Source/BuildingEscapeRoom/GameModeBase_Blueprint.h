bfc8c847bd286c2fc9989bd2466a6ffdf68b8631
PiotrMuch <piotr.muchlinski@gmail.com>
2018-05-26 17:08:26 +0200

    make a blueprints from gamemode and default pawn

---
+++ BuildingEscapeRoom/Source/BuildingEscapeRoom/GameModeBase_Blueprint.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameModeBase_Blueprint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEROOM_API AGameModeBase_Blueprint : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
